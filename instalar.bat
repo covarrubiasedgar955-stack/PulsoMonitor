195e6fcf82cf9edca8652e26448b13a34a4ae2c0
covarrubiasedgar955-stack <covarrubiasedgar955@gmail.com>
2026-08-24 11:45:43 -0600

    chore: update installer to 1.13.0

--- instalar.bat
+++ instalar.bat
@@ -5,7 +5,7 @@ cd /d "%~dp0"
 
 echo.
 echo ========================================
-echo   INSTALANDO PULSO MONITOR v1.12.1
+echo   INSTALANDO PULSO MONITOR v1.13.0
 echo ========================================
 echo.
 
